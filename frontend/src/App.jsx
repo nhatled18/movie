d1337a7930707800f3e32f496bd7335452c4ddb0
nhatled18 <147693733+nhatled18@users.noreply.github.com>
2025-10-04 15:32:50 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
-import Movies from './pages/Movies';
+import Movies from './Pages/Movies';
 import MovieDetail from './Pages/MovieDetail';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
